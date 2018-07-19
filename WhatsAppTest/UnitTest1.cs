09f0b63d3a9f5fa08bcf5375defd78f9c20a3f86
loneosama <osama.arif@khi.iba.edu.pk>
2018-07-19 20:14:26 +0500

    First Commit with tests

---
+++ WhatsAppTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace WhatsAppTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        //just testing for xunit running or not
+        public void Test1()
+        {
+            var list = new List<int>();
+            list.Add(5);
+            list.Add(10);
+            Assert.Contains(5,list);
+        }
+    }
+}
